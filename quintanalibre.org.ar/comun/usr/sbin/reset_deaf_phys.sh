fe68349b183da4f089f41b85c1b26cefc0ca609f
nicoechaniz <nicoechaniz@altermundi.net>
2017-03-30 01:15:30 -0300

    multiple fixes..

--- quintanalibre.org.ar/comun/usr/sbin/reset_deaf_phys.sh
+++ quintanalibre.org.ar/comun/usr/sbin/reset_deaf_phys.sh
@@ -2,29 +2,15 @@
 # checks for deaf interfaces and brings them up, only if there are associated stations
 
 for num in 0 1; do
-logger "deaf_phys: checking wlan$num-adhoc status..."
-
-  if [ -f /sys/kernel/debug/ieee80211/phy$num/statistics/dot11RTSSuccessCount ]; then
-#  && [ -n "$(ls -1d /sys/kernel/debug/ieee80211/phy$num/netdev*/stations/*)" ] ; then
-    ### Won't enter here if there is no phy$num or it has no stations at all
-    ### But double check: Only continue if there's any station active in the last second
-#      egrep -q "^.{1,3}$"  /sys/kernel/debug/ieee80211/phy$num/netdev*/stations/*/inactive_ms 2>/dev/null || exit
-    
-    success_a=`cat /sys/kernel/debug/ieee80211/phy$num/statistics/dot11RTSSuccessCount`
-    for fe80 in `bmx6 -c show links | grep fe80 |  grep wlan$num |  awk '{ print $2 }'`; do
-        ping6 -c 100 -i 0.1 -q -w 12  $fe80%wlan$num-adhoc
-    done
-    success_b=`cat /sys/kernel/debug/ieee80211/phy$num/statistics/dot11RTSSuccessCount`
-
-    num_all_neigh=$(ls -1d /sys/kernel/debug/ieee80211/phy$num/netdev*/stations/* | wc -l)
-    diff_success=$(( $success_b - $success_a ))
-
-    if [ $success_a == $success_b ]; then
-      netdev=$(ls -1d /sys/kernel/debug/ieee80211/phy$num/netdev* | head -n1 | cut -d \: -f 2)
-      logger "deaf_phys wlan$num-adhoc is trulala: has $num_all_neigh neighbors: 10sec diff RTSSuccess=$diff_success rebooting."
-      reboot;
-    else
-        logger "deaf_phs wlan$num-adhoc is OK: has $num_all_neigh neighbors: 10sec diff RTSSuccess=$diff_success"
+    if [ -d /sys/kernel/debug/ieee80211/phy$num/netdev\:wlan$num-adhoc ]; then
+        logger "deaf_phys: checking wlan$num-adhoc status..."
+        neighs=$(ls -ld /sys/kernel/debug/ieee80211/phy$num/netdev\:wlan$num-adhoc/stations/* 2>/dev/null | wc -l)
+        if [ $neighs == 0 ]; then
+            logger "deaf_phys: triggering wlan$num-adhoc scan..."
+            iw wlan$num-adhoc scan
+        else
+            logger "deaf_phys: wlan$num-adhoc has $neighs neighbors."
+        fi
     fi
-  fi
 done
+
