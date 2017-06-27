d9fac9225c222d565cb02c720b44a0d1035c939e
openspace <gi@al.os.vu>
2017-06-27 20:57:31 +0200

    Update 80-lime_set_11s.sh

--- openNET.io/1144-W2PA-LIME-XXXX/etc/uci-defaults/80-lime_set_11s.sh
+++ openNET.io/1144-W2PA-LIME-XXXX/etc/uci-defaults/80-lime_set_11s.sh
@@ -4,10 +4,10 @@ uci del_list lime-defaults.network.protocols=adhoc
 uci show lime-defaults.network.protocols | \
   grep -q ieee80211s || uci add_list lime-defaults.network.protocols=ieee80211s
   
-  uci del_list lime-defaults.wifi.modes=adhoc
-  uci show lime-defaults.wifi.modes | \
-    grep -q ieee80211s || uci add_list lime-defaults.wifi.modes=ieee80211s
+uci del_list lime-defaults.wifi.modes=adhoc
+uci show lime-defaults.wifi.modes | \
+  grep -q ieee80211s || uci add_list lime-defaults.wifi.modes=ieee80211s
     
-    uci commit lime-defaults
+uci commit lime-defaults
     
-    exit 0
+exit 0
