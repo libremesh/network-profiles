5b1fd3c1f641b40c3a1cb82a90bab3b78a831482
openspace <gi@al.os.vu>
2018-01-13 18:04:30 +0100

    Update on-cooker.sh

--- openNET.io/on-cooker.sh
+++ openNET.io/on-cooker.sh
@@ -1,69 +1,96 @@
 #!/bin/bash
 
-# This file is a cooking script for openNET.io
-# Execute from one level above [just outside] lime-sdk extracted folder
-# All routers we use should be in this file for one touch cooking
-#
-# Started on 20170519 | Nk
-# Last updated on 20170722 | Nk
-#
-# Packages still to test: haveged
+set -e
+set -u
 
-# Uncomment to install packages on ubuntu
+### This file is a cooking script for openNET.io
+### Execute from one level above [just outside] lime-sdk extracted folder
+### All routers we use should be in this file for one touch cooking
+### For Debian / Ubuntu [tested on Ubuntu 16]
 
-# sudo apt-get update
-# sudo apt-get -y install gawk zlib1g-dev libncurses5-dev g++ flex git
+### Started on 20170519 | Nk
+### Last updated on 20180114 | Nk
+
+### Install packages on Debian / Ubuntu
+
+sudo apt-get update
+sudo apt-get -y install subversion zlib1g-dev gawk flex unzip bzip2 gettext build-essential libncurses5-dev libncursesw5-dev libssl-dev binutils cpp psmisc docbook-to-man wget git
 
 echo
 echo "starting cook"
 echo
 
-dir=lime-sdk
-git clone https://github.com/libremesh/lime-sdk.git temp
-rsync -a temp/ $dir/
-rm -rf temp
-mv $dir/communities $dir/communities-previousrun
-mv $dir/output $dir/output-previousrun
+repo=lime-sdk
 
-cd $dir
+git clone https://github.com/libremesh/$repo.git temp
+rsync -a temp/ $repo/
+
+rm -rf temp
+
+if [ -d $repo/communities-previous_run/ ]
+then
+	rm -rf $repo/communities-previous_run/
+fi
+
+if [ -d $repo/output-previous_run/ ]
+then
+	rm -rf $repo/output-previous_run/
+fi
+
+mv $repo/communities $repo/communities-previous_run
+mv $repo/output $repo/output-previous_run
+
+cd $repo
 
 # Archer C7 v2 [standard and openNODE]
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=archer-c7-v2
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX_ONC7 -c ar71xx/generic --profile=archer-c7-v2
 
 # 1043 ND v3 [standard and openNODE]
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=tl-wr1043nd-v3
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX_ON3N -c ar71xx/generic --profile=tl-wr1043nd-v3
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX_ON32 -c ar71xx/generic --profile=tl-wr1043nd-v3
 
 # 1043 ND v4 [standard and openNODE]
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=tl-wr1043nd-v4
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX_ON3N -c ar71xx/generic --profile=tl-wr1043nd-v4
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX_ON32 -c ar71xx/generic --profile=tl-wr1043nd-v4
+
+# Unifi AC Lite
+
+./cooker --flavor=lime_default --remote \
+--community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=ubnt-unifiac-lite
+
+# Unifi AC Pro
+
+./cooker --flavor=lime_default --remote \
+--community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=ubnt-unifiac-pro
+
+# UBNT Bullet / Pico M2
+
+./cooker --flavor=lime_default --remote \
+--community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=ubnt-bullet-m
+
+# UBNT Rocket M2
+
+./cooker --flavor=lime_default --remote \
+--community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=ubnt-rocket-m
 
 # 841 v11 [4MB!]
 
@@ -73,58 +100,28 @@ PKG="wpad -wpad-mini" \
 
 # WBS 210
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=wbs210
 
-# Unifi AC Lite
-
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
-./cooker --flavor=lime_default --remote \
---community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=ubnt-unifiac-lite
-
-# Unifi AC Pro
-
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
-./cooker --flavor=lime_default --remote \
---community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=ubnt-unifiac-pro
-
 # AR 150
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=gl-ar150
 
 # AR 300
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=gl-ar300
 
 # AR 300M
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=gl-ar300m
 
 # 3020 8M
 
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
 ./cooker --flavor=lime_default --remote \
 --community=openNET.io/1144-W2PA-LIME-XXXX -c ramips/mt7620 --profile=wt3020-8M
 
-# UBNT Bullet / Pico M2
-
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
-./cooker --flavor=lime_default --remote \
---community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=ubnt-bullet-m
-
-# UBNT Rocket M2
-
-PKG="lime-full luci-ssl wpad authsae hostapd ath10k-firmware-qca988x kmod-ath10k nano -wpad-mini" \
-./cooker --flavor=lime_default --remote \
---community=openNET.io/1144-W2PA-LIME-XXXX -c ar71xx/generic --profile=ubnt-rocket-m
-
 echo "cook finished"
 echo
-
