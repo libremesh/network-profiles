00364cf4946f65acd2247105fba76d6bb189f461
a-gave <agave@dracaena.it>
2025-11-03 13:57:38 +0100

    profile.mk: do not fail if a profile's root contains more than one subdirectory

--- profile.mk
+++ profile.mk
@@ -44,7 +44,7 @@ endif
 define Package/$(PKG_NAME)/install
 	$(INSTALL_DIR) $(1)/
 	# check if root/ directory exists before copying
-	if [ -e ./root/* ]; then $(CP) -r ./root/* $(1)/; fi
+	if [ -e ./root/ ]; then $(CP) -r ./root/* $(1)/; fi
 endef
 
 define Build/Compile
